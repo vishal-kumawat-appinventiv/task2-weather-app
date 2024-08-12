5e87bc9e7cdf981d698e237503d1c56f2f794ce3
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-08-12 10:56:11 +0530

    searchBar logic added

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,23 @@ import SearchBar from "./components/SearchBar";
 import WeatherInfo from "./components/WeatherInfo";
 
 function App() {
-  useEffect(() => {
-    const fetchDetails = async () => {
-      const response = await axios.get(
-        "https://api.openweathermap.org/data/2.5/weather?q=kota&appid=3045dd712ffe6e702e3245525ac7fa38"
-      );
-      console.log("City - ", response?.data?.name);
-      console.log("country - ", response?.data?.sys?.country);
-      console.log("Temperature - ", response?.data?.main?.temp / 10);
-      console.log("Humidity - ", response?.data?.main?.humidity);
-      console.log("Wind Speed - ", response?.data?.wind?.speed);
+  // useEffect(() => {
+  //   const fetchDetails = async () => {
+  //     const response = await axios.get(
+  //       "https://api.openweathermap.org/data/2.5/weather?q=kota&appid=3045dd712ffe6e702e3245525ac7fa38"
+  //     );
+  //     console.log("City - ", response?.data?.name);
+  //     console.log("country - ", response?.data?.sys?.country);
+  //     console.log("Temperature - ", response?.data?.main?.temp / 10);
+  //     console.log("Description - ", response?.data?.weather[0]?.description);
+  //     console.log("Humidity - ", response?.data?.main?.humidity);
+  //     console.log("Wind Speed - ", response?.data?.wind?.speed);
 
-      if (!response) console.log("No Data !");
-    };
+  //     if (!response) console.log("No Data !");
+  //   };
 
-    fetchDetails();
-  }, []);
+  //   fetchDetails();
+  // }, []);
 
   return (
     <>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
 import "../Styles/searchBar.css";
-const SearchBar = () => {
+
+interface SearchProps {
+  onSearch: (cityName: string) => void;
+}
+
+const SearchBar: React.FC<SearchProps> = ({ onSearch }) => {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSearch(inputValue);
+  };
+
   return (
     <>
-      <input placeholder="City Name" />
+      <form onSubmit={handleSearch}>
+        <input
+          placeholder="City Name"
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
